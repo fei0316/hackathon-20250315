ccb9f068535b2d6b46230c38f600d18f66f932db
Fei Kuan <fei_fei_0316@hotmail.com>
2025-03-15 17:40:29 -0700

    Add files via upload

--- fun.py
+++ fun.py
@@ -2,64 +2,84 @@ import fiftyone as fo
 import fiftyone.zoo as foz
 import fiftyone.brain as fob
 import fiftyone.utils.random as four
+from ultralytics import YOLO
 
-# # A name for the dataset
-# name = "fruits"
+# A name for the dataset
+name = "fruits"
 
 # # The directory containing the dataset to import
-# dataset_dir = "/home/fei/Documents/repo/hackathon/dataset/"
+dataset_dir = "/home/fei/Documents/repo/hackathon/dataset/"
 
 # # The type of the dataset being imported
-# dataset_type = fo.types.ImageClassificationDirectoryTree  # for example
+dataset_type = fo.types.ImageClassificationDirectoryTree  # for example
 
-# dataset = fo.Dataset.from_dir(
-#     dataset_dir=dataset_dir,
-#     dataset_type=dataset_type,
-#     name=name,
-# )
+dataset = fo.Dataset.from_dir(
+    dataset_dir=dataset_dir,
+    dataset_type=dataset_type,
+    name=name,
+    persistent=True
+)
 
 # 1. Load your dataset
-dataset = fo.load_dataset("fruits")
+# dataset = fo.load_dataset("fruits")
 
 four.random_split(
     dataset, 
-    {"train": 0.7, "test": 0.2, "val": 0.1}
+    {"train": 0.17, "test": 0.03, "val": 0.8}
 )
 
 train_view = dataset.match_tags("train")
 test_view = dataset.match_tags("test")
 val_view = dataset.match_tags("val")
 
-# Check how many samples were tagged for each split
+# # Check how many samples were tagged for each split
 print("Train samples:", dataset.match_tags("train").count())
-print("Val samples:  ", dataset.match_tags("val").count())
 print("Test samples: ", dataset.match_tags("test").count())
+print("Validation samples: ", dataset.match_tags("val").count())
 
 # 6. Export the train split as an ImageClassificationDirectoryTree
 #    (one subfolder per class, as determined by `label_field`)
-# train_view.export(
-#     export_dir="/home/fei/Documents/repo/hackathon/export/train",
-#     dataset_type=fo.types.ImageClassificationDirectoryTree,
-#     label_field="ground_truth",
-# )
+train_view.export(
+    export_dir="/home/fei/Documents/repo/hackathon/datasets/train",
+    dataset_type=fo.types.ImageClassificationDirectoryTree,
+    label_field="ground_truth",
+    export_media="symlink",
+)
 
-# val_view.export(
-#     export_dir="/home/fei/Documents/repo/hackathon/export/val",
-#     dataset_type=fo.types.ImageClassificationDirectoryTree,
-#     label_field="ground_truth",
-# )
+test_view.export(
+    export_dir="/home/fei/Documents/repo/hackathon/datasets/test",
+    dataset_type=fo.types.ImageClassificationDirectoryTree,
+    label_field="ground_truth",
+    export_media="symlink",
+)
 
-# test_view.export(
-#     export_dir="/home/fei/Documents/repo/hackathon/export/test",
-#     dataset_type=fo.types.ImageClassificationDirectoryTree,
-#     label_field="ground_truth",
-# )
+val_view.export(
+    export_dir="/home/fei/Documents/repo/hackathon/datasets/val",
+    dataset_type=fo.types.ImageClassificationDirectoryTree,
+    label_field="ground_truth",
+    export_media="symlink",
+)
 
-# 7. (Optional) Do the same for val/test if needed
-# val_view.export(...)
-# test_view.export(...)
+# The path to the `dataset.yaml` file we created above
+# YAML_FILE = "/home/fei/Documents/repo/hackathon/datasets/"
+
+# Load a model
+model = YOLO("yolo11n-cls.pt")  # load a pretrained model
+# model = YOLO("yolov8s.yaml")  # build a model from scratch
+
+# Train the model
+model.train(data="/home/fei/Documents/repo/hackathon/datasets/", epochs=3,
+                    imgsz=320, batch=12, project="fruits-yolo", exist_ok=True)
+
+# Evaluate model on the validation set
+metrics = model.val()
+
+# Export the model
+path = model.export(format="onnx")
+
+
 
 # 8. Launch the FiftyOne App to see your data
-session = fo.launch_app(dataset)
-session.wait()
+# session = fo.launch_app(dataset)
+# session.wait()
 
